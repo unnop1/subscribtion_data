1e658c06a60249a683ba6c18ddd1f4475dbfa6c7
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-06-02 23:31:44 +0700

    update kafka host credentail

--- src/main/java/com/nt/subscribtion_data/config/KafkaProducerConfig.java
+++ src/main/java/com/nt/subscribtion_data/config/KafkaProducerConfig.java
@@ -24,6 +24,9 @@ public class KafkaProducerConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put("security.protocol", "SASL_PLAINTEXT");
+        configProps.put("sasl.mechanism", "SCRAM-SHA-256");
+        configProps.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"admin\" password=\"admin-secret\";");
         return new DefaultKafkaProducerFactory<>(configProps);
     }
     @Bean
